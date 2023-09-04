3ae19b87cfa2c89f98b542ef5fd83299eea364f6
safeer <you@example.com>
2023-09-04 09:29:10 +0500

    loginregister

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+
+</x-app-layout>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    admin dahsboard
+</body>
+</html>
---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+
+</x-app-layout>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    user dashboard
+</body>
+</html>
